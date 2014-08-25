ab4689bbceacb4755c8750e040c545b6631f7cf9
Ben Burdette <bburdette@gmail.com>
2014-08-25 14:34:09 -0600

    fix a couple things

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 let pkgs = import <nixpkgs> {};
     haskellPackages = pkgs.haskellPackages.override {
       extension = self: super: {
-        scoscdir = self.callPackage ./. {};
+        hackstarter = self.callPackage ./. {};
       };
     };
- in pkgs.lib.overrideDerivation haskellPackages.scoscdir (attrs: {
+ in pkgs.lib.overrideDerivation haskellPackages.hackstarter (attrs: {
    buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
  })
