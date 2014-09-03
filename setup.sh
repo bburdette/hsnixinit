fbbba97df335f55b386f05c0b0c3a089b1bc7d7c
Ben Burdette <bburdette@gmail.com>
2014-09-03 09:37:37 -0600

    extra nixpkgs arg to setup.sh causes it to make an 'ns' script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,16 @@
 #  this should create a default.nix and shell.nix.
 #  nix-shell should then pull in the dependencies if they are in your nixpkgs.
 #
+#  You can add a nixpkgs parameter and the script will make a 'ns' script so you 
+#  don't have to type "nix-shell -I nixpkgs=~/mynixpkgs" every time.
+#
+#  for example: 
+#  [nixuser@nixsys myproject]$ ~/hsnixinit/setup.sh myproject ~/nixpkgs14.04
+#  will make a 'ns' script that contains: 
+#      "nix-shell -I nixpkgs=home/username/nixpkgs14.04"
+#
+#
+#
 INSTDIR=$(dirname $0)
 SHELLNIX="${INSTDIR}/shell.nix"
 SHELLDEST="${PWD}/shell.nix"
@@ -14,3 +24,9 @@ cp $SHELLNIX $SHELLDEST
 sed -i "s/hackstarter/$1/g" shell.nix
 cabal2nix $1.cabal --sha256=0 > default.nix
 sed -i 's/sha256 = "0"/src=.\/./g' default.nix
+
+if [ "$#" -gt "1" ]
+  then 
+    echo "nix-shell -I nixpkgs=$2" > ns
+    chmod +x ns
+fi
